444f83b906007c35dc23942ca7b99985cdf52eb9
jotap <jpleite948@gmail.com>
2026-08-15 12:22:13 -0300

    BootStrap e js separado

---
+++ app.js
@@ -0,0 +1,25 @@
+let salarioPrincipal = 0;
+        let fontes = [];
+        let despesas = 0;
+
+        function salvarNome(){
+            const nome = document.getElementById('nome').value;
+            document.getElementById('resultado').textContent = `Olá, ${nome}`;
+        }
+
+        function salvarSalario(){
+            const salario = document.getElementById('salario').value;
+            salarioPrincipal = Number(salario);
+            document.getElementById('resultadoSalario').textContent = `Salario R$${salarioPrincipal.toFixed(2)}`;
+            calcularTotal()
+        }
+
+        function salvarGastos(){
+            const gastostotal = document.getElementById('gastos').value;
+            despesas = Number(gastostotal);
+            document.getElementById('resultadoGastos').textContent = `Seus Gastos: R$${despesas.toFixed(2)}`;
+        }
+
+        function montanteFinal(){
+            salarioPrincipal
+        }
