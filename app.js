63bc2051363aeac0f55cc6a9a7b1719f1142bbd8
Crhystian <crhysz1223@gmail.com>
2026-08-15 17:01:37 -0300

    Montante Final ( Salario - Despesas ) na tela

--- app.js
+++ app.js
@@ -1,28 +1,31 @@
 let salarioPrincipal = 0;
-        let fontes = [];
-        let despesas = 0;
+let fontes = [];
+let despesas = 0;
 
-        function salvarNome(){
-            const nome = document.getElementById('nome').value;
-            document.getElementById('resultado').textContent = `Olá, ${nome}`;
-        }
+function salvarNome(){
+    const nome = document.getElementById('nome').value;
+    document.getElementById('resultado').textContent = `Olá, ${nome}`;
+}
 
-        function salvarSalario(){
-            const salario = document.getElementById('salario').value;
-            salarioPrincipal = Number(salario);
-            document.getElementById('resultadoSalario').textContent = `Salario R$${salarioPrincipal.toFixed(2)}`;
-            calcularTotal()
-        }
+function salvarSalario(){
+    const salario = document.getElementById('salario').value;
+    salarioPrincipal = Number(salario);
+    document.getElementById('resultadoSalario').textContent = `Salario R$${salarioPrincipal.toFixed(2)}`;
+    montanteFinal()
+}
 
-        function salvarGastos(){
-            const gastostotal = document.getElementById('gastos').value;
-            despesas = Number(gastostotal);
-            document.getElementById('resultadoGastos').textContent = `Seus Gastos: R$${despesas.toFixed(2)}`;
-        }
+function salvarGastos(){
+    const gastostotal = document.getElementById('gastos').value;
+    despesas = Number(gastostotal);
+    document.getElementById('resultadoGastos').textContent = `Seus Gastos: R$${despesas.toFixed(2)}`;
+    montanteFinal()
+}
 
-        function montanteFinal(){
-            salarioPrincipal
-        }
+function montanteFinal(){
+    const user = document.getElementById('nome').value;
+    let restante = salarioPrincipal - despesas;
+    document.getElementById('dinheiroRestante').textContent = `${user}, vai restar R$${restante.toFixed(2)} este mês.`;
+}
 
 
 //TESTE Modal Bootstrap (Pop-up)
