417b58070590de1bd8f94a09092c9996494e2a86
Crhystian <crhysz1223@gmail.com>
2026-08-16 19:05:53 -0300

    Modal e a página mostram o valor total das despesas e mostra todas as despesas registradas.

--- app.js
+++ app.js
@@ -23,16 +23,17 @@ function salvarGastos(){
         valor: valorDespesa
     };
     despesas.push(novaDespesa); //Adicionar na array tlgd
+    const total = despesas.reduce((soma, despesa) => soma + despesa.valor, 0);
     const listaDespesas = document.getElementById('listaDespesas'); //seelecionando a div 
-    listaDespesas.innerHTML = 'Suas Despesas:<br>'; // apareeceer na div listaDespesas
+    listaDespesas.innerHTML = `Total em Despesas: R$${total.toFixed(2)}<br>Suas Despesas:<br>`; // apareeceer na div listaDespesas
     despesas.forEach(function(despesa) { // percorrer a array e exibir na div
         listaDespesas.innerHTML += `${despesa.tipo} | R$ ${despesa.valor.toFixed(2)}<br>`;
     });
-    //
-    const total = despesas.reduce((soma, despesa) => soma + despesa.valor, 0);
-
-    document.getElementById('resultadoGastos').innerHTML = `Total em Despesas: R$${total.toFixed(2)}<br>Suas Despesas:<br>${novaDespesa.tipo} | R$ ${novaDespesa.valor.toFixed(2)}<br>`;
-
+    const listaDespesas2 = document.getElementById('resultadoGastos');
+    listaDespesas2.innerHTML = `Total em Despesas: R$${total.toFixed(2)}<br>Suas Despesas:<br>`;
+    despesas.forEach(function(despesa){
+        listaDespesas2.innerHTML += `${despesa.tipo} | R$ ${despesa.valor.toFixed(2)}<br>`;
+    });
     montanteFinal();
 }
 
@@ -43,8 +44,6 @@ function montanteFinal(){
     let restante = salarioPrincipal - totalDespesas;
     document.getElementById('dinheiroRestante').innerHTML = `Seu Salário: R$${salario}<br>${user}, vai restar R$ ${restante.toFixed(2)} do seu salário este mês.`;
 }
-
-
 
 //TESTE Modal Bootstrap (Pop-up)
 
